de7f3693b4a34201fc23c863391ef0a96b9133f1
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2016-08-04 17:37:58 +0900

    cat works

---
+++ user/cat.c
@@ -0,0 +1,15 @@
+#include "noah.h"
+
+int _start()
+{
+  char buf[256];
+  long n;
+  while ((n = read(0, buf, sizeof buf)) > 0) {
+    long offset = 0, w;
+    while ((w = write(1, buf + offset, n)) > 0) {
+      offset += w;
+      n -= w;
+    }
+  }
+  _exit(0);
+}
--- user/noah.h
+++ user/noah.h
@@ -2,9 +2,9 @@ typedef unsigned long uint64_t;
 typedef unsigned long size_t;
 typedef long ssize_t;
 
-int syscall(uint64_t num, uint64_t rdi, uint64_t rsi, uint64_t rdx)
+uint64_t syscall(uint64_t num, uint64_t rdi, uint64_t rsi, uint64_t rdx)
 {
-  int ret;
+  uint64_t ret;
   asm("syscall"
       : "=rax" (ret)
       : "0" (num), "D" (rdi), "S" (rsi), "d" (rdx)
@@ -13,12 +13,18 @@ int syscall(uint64_t num, uint64_t rdi, uint64_t rsi, uint64_t rdx)
   return ret;
 }
 
+#define SYS_read 0
 #define SYS_write 1
 #define SYS_exit 60
 
-void write(int fd, const void *buf, size_t count)
+ssize_t read(int fd, void *buf, size_t count)
 {
-  syscall(SYS_write, fd, (uint64_t)buf, count);
+  return syscall(SYS_read, fd, (uint64_t)buf, count);
+}
+
+ssize_t write(int fd, const void *buf, size_t count)
+{
+  return syscall(SYS_write, fd, (uint64_t)buf, count);
 }
 
 void _exit(int status)
