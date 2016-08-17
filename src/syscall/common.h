f40a4c5507266d6ea4f745d2befe92a6ac595a2c
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2016-08-17 23:58:21 +0900

    cosmetic changes

--- src/syscall/common.h
+++ src/syscall/common.h
@@ -1,36 +1,34 @@
 #include <stdint.h>
 #include "syscall.h"
 
-#define KV_SEL(K0,V0,K1,V1,K2,V2,K3,V3,K4,V4,K5,V5,X,...) X
-
 /*
- * ``MAP_KV(f, key0, val0, key1, val1, ...)'' expands into ``f(key0, val0), f(key1, val1), ...''
+ * ``_MAP(f, t0, v0, t1, v1, ...)'' => ``f(t0, v0), f(t1, v1), ...''
  */
 
-#define MAP_KV(f,...) MAP_KV_HELP(KV_SEL(__VA_ARGS__,MAP_KV6,ERROR,MAP_KV5,ERROR,MAP_KV4,ERROR,MAP_KV3,ERROR,MAP_KV2,ERROR,MAP_KV1,MAP_KV0),f,__VA_ARGS__)
-#define MAP_KV_HELP(MAP_KVN,f,...) MAP_KVN(f,__VA_ARGS__)
-#define MAP_KV0(f,...)
-#define MAP_KV1(f,k,v)     f(k,v)
-#define MAP_KV2(f,k,v,...) f(k,v), MAP_KV1(f,__VA_ARGS__)
-#define MAP_KV3(f,k,v,...) f(k,v), MAP_KV2(f,__VA_ARGS__)
-#define MAP_KV4(f,k,v,...) f(k,v), MAP_KV3(f,__VA_ARGS__)
-#define MAP_KV5(f,k,v,...) f(k,v), MAP_KV4(f,__VA_ARGS__)
-#define MAP_KV6(f,k,v,...) f(k,v), MAP_KV5(f,__VA_ARGS__)
+#define _MAP(f,...) _DISPATCH(__VA_ARGS__,_MAP6,,_MAP5,,_MAP4,,_MAP3,,_MAP2,,_MAP1,_MAP0)(f,__VA_ARGS__)
+#define _MAP0(f,...)
+#define _MAP1(f,t,v)     f(t,v)
+#define _MAP2(f,t,v,...) f(t,v), _MAP1(f,__VA_ARGS__)
+#define _MAP3(f,t,v,...) f(t,v), _MAP2(f,__VA_ARGS__)
+#define _MAP4(f,t,v,...) f(t,v), _MAP3(f,__VA_ARGS__)
+#define _MAP5(f,t,v,...) f(t,v), _MAP4(f,__VA_ARGS__)
+#define _MAP6(f,t,v,...) f(t,v), _MAP5(f,__VA_ARGS__)
+#define _DISPATCH(T0,V0,T1,V1,T2,V2,T3,V3,T4,V4,T5,V5,X,...) X
 
 #define MK_DECL(t,v) t v
 #define MK_TEMP(t,v) uint64_t temp__##v
 #define MK_CAST(t,v) (t) temp__##v
 
 #define DECLARE_SCFUNCT(name, ...)                      \
-  uint64_t _sys_##name(MAP_KV(MK_DECL, __VA_ARGS__));
+  uint64_t _sys_##name(_MAP(MK_DECL, __VA_ARGS__));
 
 #define DEFINE_SCWRAPPER(name, ...)                     \
-  uint64_t sys_##name(MAP_KV(MK_TEMP,__VA_ARGS__)) {    \
-    return _sys_##name(MAP_KV(MK_CAST,__VA_ARGS__));    \
+  uint64_t sys_##name(_MAP(MK_TEMP,__VA_ARGS__)) {    \
+    return _sys_##name(_MAP(MK_CAST,__VA_ARGS__));    \
   }
 
 #define DEFINE_SCFUNCT(name, ...)                       \
-  uint64_t _sys_##name(MAP_KV(MK_DECL, __VA_ARGS__))
+  uint64_t _sys_##name(_MAP(MK_DECL, __VA_ARGS__))
 
 #define DEFINE_SYSCALL(name, ...)               \
   DECLARE_SCFUNCT(name, __VA_ARGS__)            \
