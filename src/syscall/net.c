9c6b4a2513e4a54fe9ae8b1b8c7b0d5165c13f6d
nullpo-head <abc.tkys+pub@gmail.com>
2016-08-24 15:20:13 +0900

    Implement setsockopt

--- src/syscall/net.c
+++ src/syscall/net.c
@@ -11,22 +11,28 @@
 #include "linux/socket.h"
 #include "linux/misc.h"
 
-DEFINE_SYSCALL(socket, int, family, int, type, int, protocol)
+int
+to_host_sa_family(int family)
 {
-  int nfamily;
   switch (family) {
-  case LINUX_AF_UNIX: nfamily = AF_UNIX; break;
-  case LINUX_AF_INET: nfamily = AF_INET; break;
-  case LINUX_AF_INET6: nfamily = AF_INET6; break;
+  case LINUX_AF_UNIX:
+    return AF_UNIX;
+  case LINUX_AF_INET:
+    return AF_INET;
+  case LINUX_AF_INET6:
+    return AF_INET6;
   default:
     return -1;
   }
+}
 
+DEFINE_SYSCALL(socket, int, family, int, type, int, protocol)
+{
   /* FIXME */
   type &= ~LINUX_SOCK_NONBLOCK;
   type &= ~LINUX_SOCK_CLOEXEC;
 
-  return socket(nfamily, type, protocol);
+  return socket(to_host_sa_family(family), type, protocol);
 }
 
 DEFINE_SYSCALL(connect, int, sockfd, gaddr_t, addr, uint64_t, addrlen)
@@ -36,13 +42,7 @@ DEFINE_SYSCALL(connect, int, sockfd, gaddr_t, addr, uint64_t, addrlen)
 
   memcpy(buf, guest_to_host(addr), addrlen);
 
-  switch (naddr->sa_family) {
-  case LINUX_AF_UNIX: naddr->sa_family = AF_UNIX; break;
-  case LINUX_AF_INET: naddr->sa_family = AF_INET; break;
-  case LINUX_AF_INET6: naddr->sa_family = AF_INET6; break;
-  default:
-    return -1;
-  }
+  naddr->sa_family = to_host_sa_family(naddr->sa_family);
 
   errno = 0;
   int fd = connect(sockfd, naddr, addrlen);
@@ -50,9 +50,62 @@ DEFINE_SYSCALL(connect, int, sockfd, gaddr_t, addr, uint64_t, addrlen)
   return fd;
 }
 
-DEFINE_SYSCALL(setsockopt, int, fd, int, level, int, optname, gaddr_t, optval, int, option)
+int
+to_host_sockopt_level(int level)
 {
-  return -1;
+  switch (level) {
+  case LINUX_SOL_SOCKET: return SOL_SOCKET;
+  default: return level; // Other values are the same as OSX as long as they exist.
+  };
+}
+
+int
+to_host_sockopt_name(int name)
+{
+  switch (name) {
+  case LINUX_SO_DEBUG:
+    return SO_DEBUG;
+  case LINUX_SO_REUSEADDR:
+    return SO_REUSEADDR;
+  case LINUX_SO_TYPE:
+    return SO_TYPE;
+  case LINUX_SO_ERROR:
+    return SO_ERROR;
+  case LINUX_SO_DONTROUTE:
+    return SO_DONTROUTE;
+  case LINUX_SO_BROADCAST:
+    return SO_BROADCAST;
+  case LINUX_SO_SNDBUF:
+    return SO_SNDBUF;
+  case LINUX_SO_RCVBUF:
+    return SO_RCVBUF;
+  case LINUX_SO_KEEPALIVE:
+    return SO_KEEPALIVE;
+  case LINUX_SO_OOBINLINE:
+    return SO_OOBINLINE;
+  case LINUX_SO_LINGER:
+    return SO_LINGER;
+  case LINUX_SO_RCVLOWAT:
+    return SO_RCVLOWAT;
+  case LINUX_SO_SNDLOWAT:
+    return SO_SNDLOWAT;
+  case LINUX_SO_RCVTIMEO:
+    return SO_RCVTIMEO;
+  case LINUX_SO_SNDTIMEO:
+    return SO_SNDTIMEO;
+  case LINUX_SO_TIMESTAMP:
+    return SO_TIMESTAMP;
+  case LINUX_SO_ACCEPTCONN:
+    return SO_ACCEPTCONN;
+  default:
+    PRINTF("Unsupported sockopt name: 0x%x\n", name);
+    return -1;
+  }
+}
+
+DEFINE_SYSCALL(setsockopt, int, fd, int, level, int, optname, gaddr_t, optval, int, opt_len)
+{
+  return setsockopt(fd, to_host_sockopt_level(level), to_host_sockopt_name(optname), guest_to_host(optval), opt_len);
 }
 
 DEFINE_SYSCALL(getsockopt, int, fd, int, level, int, optname, gaddr_t, optval, gaddr_t, option)
