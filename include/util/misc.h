bc5e480bf0a4b63c0d470d0573e5515ce42d50fd
nullpo-head <abc.tkys+pub@gmail.com>
2016-12-04 18:48:22 +0900

    Add is_aligned helper

--- include/util/misc.h
+++ include/util/misc.h
@@ -1,4 +1,5 @@
 #include <stdint.h>
+#include <stdbool.h>
 
 #define MIN(a,b) (((a)<(b))?(a):(b))
 #define MAX(a,b) (((a)>(b))?(a):(b))
@@ -9,4 +10,8 @@ static inline uint64_t rounddown(uint64_t x, uint64_t y) {
 
 static inline uint64_t roundup(uint64_t x, uint64_t y) {
   return (x + y - 1) / y * y;
+}
+
+static inline bool is_aligned(uint64_t x, uint64_t y) {
+  return rounddown(x, y) == x;
 }
