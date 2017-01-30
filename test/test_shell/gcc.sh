8790b1df4698613974774eda5a08db19d70fbbde
nullpo-head <abc.tkys+pub@gmail.com>
2017-01-30 16:57:11 +0900

    Improve the incomplete fix on f7c7a35

--- test/test_shell/gcc.sh
+++ test/test_shell/gcc.sh
@@ -1,4 +1,4 @@
-HELLO=`mktemp /tmp/noah-hello.XXXXXX`.c
+HELLO=`mktemp noah-hello.XXXXXX`.c
 cat << EOF > $HELLO
 #include <stdio.h>
 
@@ -6,7 +6,9 @@ int main(){
   printf("hello, world!");
 }
 EOF
-BIN=`mktemp /tmp/noah-hello.XXXXXX`
+BIN=`mktemp noah-hello.XXXXXX`
 $NOAH /usr/bin/gcc $HELLO -o $BIN
-test -s $BIN -a "`$NOAH $BIN`" = "hello, world!" || exit 1
-
+test -s $BIN -a "`$NOAH $BIN`" = "hello, world!"
+RET=$?
+rm -f $HELLO $BIN
+exit $RET
--- test/test_shell/mv.sh
+++ test/test_shell/mv.sh
@@ -2,5 +2,7 @@ TEMP1=`mktemp noah-mv.XXXXXX`
 TEMP2=`mktemp noah-mv.XXXXXX`
 echo "hello world" > $TEMP1
 $NOAH $TARGET $TEMP1 $TEMP2
-test ! -f $TEMP1 -a -f $TEMP2 -a "`cat $TEMP2`" = "hello world" || exit 1
+test ! -f $TEMP1 -a -f $TEMP2 -a "`cat $TEMP2`" = "hello world"
+RET=$?
 rm -f $TEMP2
+exit $RET
