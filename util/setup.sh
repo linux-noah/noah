2afe7a54d187fc3dd4415931d7a238173c110939
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2017-02-03 21:36:06 +0900

    setup.h supports optional command line argument

--- util/setup.sh
+++ util/setup.sh
@@ -2,9 +2,11 @@
 
 set -eu
 
-sudo sh -c "./mkgroup.pl | sort | uniq > ~/.noah/tree/etc/group"
-sudo sh -c "./mkpasswd.pl | sort | uniq > ~/.noah/tree/etc/passwd"
+TREE_ROOT=${1-~/.noah/tree}
 
-sudo chmod u+w ~/.noah/tree/etc/sudoers
-sudo sh -c 'echo "yuichi ALL=(ALL) NOPASSWD: ALL" >> ~/.noah/tree/etc/sudoers'
-sudo chmod u-w ~/.noah/tree/etc/sudoers
+sudo sh -c "./mkgroup.pl | sort | uniq > $TREE_ROOT/etc/group"
+sudo sh -c "./mkpasswd.pl | sort | uniq > $TREE_ROOT/etc/passwd"
+
+sudo chmod u+w $TREE_ROOT/etc/sudoers
+sudo sh -c "echo 'yuichi ALL=(ALL) NOPASSWD: ALL' >> $TREE_ROOT/etc/sudoers"
+sudo chmod u-w $TREE_ROOT/etc/sudoers
