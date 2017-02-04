92e839e0fab75518ebde8bacbe455699ff568089
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2017-02-04 20:32:55 +0900

    sizes of l_timer_t and l_socklen_t are wrong

--- include/linux/common.h
+++ include/linux/common.h
@@ -56,6 +56,48 @@ typedef uint16_t	l_ushort;
 
 typedef l_ulong		l_uintptr_t;
 typedef l_ulong		l_ucharptr_t;
+
+/**
+// The following typedefs are generated automatially by:
+#include <sys/socket.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <mqueue.h>
+#include <stdio.h>
+
+#define ISUNSIGNED(type) ((type)0 - 1 > 0)
+#define P(type) do {                                    \
+  char *ty = 0;                                         \
+  switch (sizeof(type)) {                               \
+  case 2: ty = "short"; break;                          \
+  case 4: ty = "int"; break;                            \
+  case 8: ty = "long"; break;                           \
+  }                                                     \
+  printf("typedef l_%s%s\t\tl_%s;\n", (ISUNSIGNED(type) ? "u" : ""), ty, #type);  \
+} while (0)
+
+int main() {
+  P(clock_t);
+  P(daddr_t);
+  P(dev_t);
+  P(gid_t);
+  P(uid_t);
+  P(ino_t);
+  P(key_t);
+  P(loff_t);
+  P(mode_t);
+  P(off_t);
+  P(pid_t);
+  P(size_t);
+  P(ssize_t);
+  P(suseconds_t);
+  P(time_t);
+  P(timer_t);
+  P(mqd_t);
+  P(socklen_t);
+  P(fd_mask);
+}
+ */
 typedef l_long		l_clock_t;
 typedef l_int		l_daddr_t;
 typedef l_ulong		l_dev_t;
@@ -71,10 +113,10 @@ typedef l_ulong		l_size_t;
 typedef l_long		l_ssize_t;
 typedef l_long		l_suseconds_t;
 typedef l_long		l_time_t;
-typedef l_int		l_timer_t;
+typedef l_ulong		l_timer_t;
 typedef l_int		l_mqd_t;
-typedef l_size_t	l_socklen_t;
-typedef	l_ulong		l_fd_mask;
+typedef l_uint		l_socklen_t;
+typedef l_long		l_fd_mask;
 
 #define	LINUX_NAME_MAX		255
 
