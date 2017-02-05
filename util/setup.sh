81f64af472c9a16acd75b9474680a5013c146777
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2017-02-05 22:42:16 +0900

    setup.sh: use abs path to refer to the aux scripts

--- util/setup.sh
+++ util/setup.sh
@@ -3,9 +3,10 @@
 set -eu
 
 TREE_ROOT=${1-~/.noah/tree}
+SCRIPT_DIR=`dirname $0`
 
-sudo sh -c "./mkgroup.pl | sort | uniq > $TREE_ROOT/etc/group"
-sudo sh -c "./mkpasswd.pl | sort | uniq > $TREE_ROOT/etc/passwd"
+sudo sh -c "$SCRIPT_DIR/mkgroup.pl | sort | uniq > $TREE_ROOT/etc/group"
+sudo sh -c "$SCRIPT_DIR/mkpasswd.pl | sort | uniq > $TREE_ROOT/etc/passwd"
 
 sudo chmod u+w $TREE_ROOT/etc/sudoers
 sudo sh -c "echo 'yuichi ALL=(ALL) NOPASSWD: ALL' >> $TREE_ROOT/etc/sudoers"
