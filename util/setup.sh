54b6efa49054f1e9cf6ecb1c190de1174c0a4cfe
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2017-02-05 22:44:23 +0900

    setup.sh: fix up /etc/resolv.conf

--- util/setup.sh
+++ util/setup.sh
@@ -11,3 +11,5 @@ sudo sh -c "$SCRIPT_DIR/mkpasswd.pl | sort | uniq > $TREE_ROOT/etc/passwd"
 sudo chmod u+w $TREE_ROOT/etc/sudoers
 sudo sh -c "echo 'yuichi ALL=(ALL) NOPASSWD: ALL' >> $TREE_ROOT/etc/sudoers"
 sudo chmod u-w $TREE_ROOT/etc/sudoers
+
+sudo sh -c "cat /etc/resolv.conf >> $TREE_ROOT/etc/resolv.conf"
