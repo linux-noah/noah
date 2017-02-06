4601bb2b4925142854692c52f3968be34839f12d
nullpo-head <abc.tkys+pub@gmail.com>
2017-02-06 22:58:09 +0900

    Fix the failure of the gcc_pic test case

--- test/test_shell/gcc_pic.sh
+++ test/test_shell/gcc_pic.sh
@@ -1,12 +1,14 @@
-HELLO=`mktemp /tmp/noah-hello.XXXXXX`.c
-cat << EOF > $HELLO
+HELLO=`mktemp noah-hello.XXXXXX`
+cat << EOF > $HELLO.c
 #include <stdio.h>
 
 int main(){
   printf("hello, world!");
 }
 EOF
-BIN=`mktemp /tmp/noah-hello.XXXXXX`
-$NOAH /usr/bin/gcc -fPIC -pie $HELLO -o $BIN
-test -s $BIN -a "`$NOAH $BIN`" = "hello, world!" || exit 1
-
+BIN=`mktemp noah-hello.XXXXXX`
+$NOAH /usr/bin/gcc -fPIC -pie $HELLO.c -o $BIN
+test -s $BIN -a "`$NOAH $BIN`" = "hello, world!"
+RET=$?
+rm -f $HELLO $HELLO.c $BIN
+exit $RET
