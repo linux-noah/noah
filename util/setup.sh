51dd24987ccba6f170fd0e8305874d83d769db93
nullpo-head <abc.tkys+pub@gmail.com>
2017-02-17 22:05:18 +0900

    s/yuichi/$USER/

--- util/setup.sh
+++ util/setup.sh
@@ -9,7 +9,7 @@ sudo sh -c "$SCRIPT_DIR/mkgroup.pl | sort | uniq > $TREE_ROOT/etc/group"
 sudo sh -c "$SCRIPT_DIR/mkpasswd.pl | sort | uniq > $TREE_ROOT/etc/passwd"
 
 sudo chmod u+w $TREE_ROOT/etc/sudoers
-sudo sh -c "echo 'yuichi ALL=(ALL) NOPASSWD: ALL' >> $TREE_ROOT/etc/sudoers"
+sudo sh -c "echo '$USER ALL=(ALL) NOPASSWD: ALL' >> $TREE_ROOT/etc/sudoers"
 sudo chmod u-w $TREE_ROOT/etc/sudoers
 
 sudo sh -c "cat /etc/resolv.conf >> $TREE_ROOT/etc/resolv.conf"
